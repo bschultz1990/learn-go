fc2c252144443ec789808f5bba2acc0ec0dfa5c9
Ben <bschultz1990@gmail.com>
2025-11-08 19:50:15 -0600

    More functions!

--- booking-app/main.go
+++ booking-app/main.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+
 func namevalidation(fn string, ln string) bool {
 	if len(fn) < 2 || len(ln) < 2 {
 		fmt.Println("First name and last name must be at least two characters long. Try again.")
@@ -13,14 +14,32 @@ func namevalidation(fn string, ln string) bool {
 	return true
 }
 
+
+func greetUsers(confName string, remTickets uint, confTickets uint,) {
+	fmt.Println("Welcome to the", confName, "booking application!")
+	fmt.Println("There are", remTickets,"out of", confTickets, "tickets left.")
+	fmt.Println("Get your tickets here to attend.")
+}
+
+
+func printFirstNames(bkngs []string) []string {
+		firstNames := []string{}
+		for _, booking := range bkngs {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		return firstNames
+}
+
+
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets uint = 50
 	var remainingTickets uint = 50
 
-	fmt.Println("Welcome to the", conferenceName, "booking application!")
-	fmt.Println("There are", remainingTickets,"out of", conferenceTickets, "tickets left.")
-	fmt.Println("Get your tickets here to attend.")
+
+	greetUsers(conferenceName, remainingTickets, conferenceTickets)
+
 
 	var bookings = []string{}
 
@@ -32,20 +51,17 @@ func main() {
 
 		fmt.Println("\nEnter your first name: ")
 		fmt.Scan(&firstName)
-
 		fmt.Println("\nEnter your last name: ")
 		fmt.Scan(&lastName)
 		if namevalidation(firstName, lastName) == false {
 			continue
 		}
-
 		fmt.Println("\nEnter your email: ")
 		fmt.Scan(&email)
 		if ! strings.Contains(email, "@") {
 			fmt.Println("Please enter a valid email address format.")
 			continue
 		}
-
 		fmt.Println("\nEnter number of tickets: ")
 		fmt.Scan(&userTickets)
 
@@ -63,15 +79,8 @@ func main() {
 
 		fmt.Printf("%v out of %v tickets remaining.\n", remainingTickets, conferenceTickets)
 
-		firstNames := []string{}
-		for _, booking := range bookings {
-
-			// Split each element of the array on a space.
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
-
-		fmt.Printf("\nHere are all the first names who booked: %v\n", firstNames)
+		firstNames := printFirstNames(bookings)
+		fmt.Printf("Here are the first names for all who booked: %v", firstNames)
 
 		if remainingTickets == 0 {
 			fmt.Println("No more tickets available. Thank you for playing!")
